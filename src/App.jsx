4df1f24cbbb4b1c97453664ee741e0512e4246fb
Bea Sofia Balauro <111563107+BaeSuii@users.noreply.github.com>
2023-10-07 13:20:59 +0000

    Updated src components

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Bea Sofia Balauro",
+  title: "Software Engineer & Technical Animator",
+  email: "baesuii.arts@gmail.com",
   gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  instagram: "_baesuii",
+  linkedIn: "bea-sofia-b-69a67a184",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,20 +24,13 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Software Engineer student studying Bachelor in Information Technology at Cap College. I am also a full time Technical Animator. I enjoy creating games, apps and websites in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
-];
+const skillsList = ["Software Development", "Game Development", "Design", "Animation"];
 
 /**
  * Use this to give more information about what you are passionate about,
